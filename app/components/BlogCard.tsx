509b96e00d8a0fdf293ef4feaa2ef35c80e4d041
Ali Irani <ali.irani@ovou.com>
2025-01-20 17:45:00 +0330

    Enhance BlogCard component with new design and content
    - Updated BlogCard to include a new image and improved hover effects for better user interaction.
    - Added a detailed content section in Persian, highlighting recent volunteer activities and contributions in the region.
    - Adjusted styling for better visual appeal and readability.
    
    These changes improve the overall presentation and user engagement of the BlogCard component.

--- app/components/BlogCard.tsx
+++ app/components/BlogCard.tsx
@@ -6,11 +6,15 @@ import Image from "next/image";
 import Link from "next/link";
 function BlogCard() {
   return (
-    <Card className="w-[600px]">
-      <CardHeader className="flex flex-row justify-center">
-        <Image src="/next.svg" alt="Blog Image" width={600} height={600} />
-        {/* <CardTitle>Card Title</CardTitle>
-        <CardDescription>Card Description</CardDescription> */}
+    <Card className="w-[600px] transition-all duration-300 ease-in-out hover:scale-[1.02] hover:shadow-lg cursor-pointer">
+      <CardHeader className="p-0 mb-5 h-[300px] shadow">
+        <Image
+          src="/book-card-mock.png"
+          alt="Blog Image"
+          width={600}
+          height={600}
+          className="w-full h-full object-cover rounded-t-xl shadow"
+        />
       </CardHeader>
       <CardContent>
         <div className="flex flex-row justify-between">
@@ -22,9 +26,16 @@ function BlogCard() {
             <p className="mx-auto">دسته‌بندی گزارش</p>
           </Badge>
         </div>
-        <div className="my-10">محتوا</div>
+        <div className="my-5 text-lg text-justify">
+          <p>
+            در آذرماه ۱۴۰۳ گروه داوطلبین ما در شهرهای خراسان جنوبی و روستاهای
+            تابعه آن‌ها به فعالیت پرداختند. در این دوره از فعالیت گروه داوطلبین
+            بی‌مرز تعداد ۲۵۰ کتاب نو تحویل مدارس منطقه شد و ۵ کتابخانه برای
+            دانش‌آموزان منطقه تجهیز شد و ...
+          </p>
+        </div>
       </CardContent>
-      <CardFooter className="flex flex-row justify-end text-[#671D57] mb-[-10px]">
+      <CardFooter className="flex flex-row justify-end text-[#671D57] mb-[-10px] font-bold text-xl -mt-[20px]">
         <Link href="/blog/1">- بخوانید -</Link>
       </CardFooter>
     </Card>
